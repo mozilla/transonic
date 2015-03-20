1a25d0ef97433f4686cb7fc2f97725515f0b2c2f
Kevin Ngo <ngoke@mozilla.com>
2015-03-20 12:36:32 -0700

    helpers_local back in init but after settings_app

--- src/media/js/init.js
+++ src/media/js/init.js
@@ -3,8 +3,10 @@
     Exposes a promise that the `main` module should wait on.
 */
 define('init',
-    ['core/init', 'routes', 'settings_app', 'templates'],
-    function(init, routes, settingsApp, templates) {
+    ['core/init', 'routes', 'settings_app', 'templates',
+     'helpers_local'],
+    function(init, routes, settingsApp, templates,
+             helpers_local) {
 
     return init.ready;
 });
